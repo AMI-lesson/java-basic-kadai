b854416ec338aaa99e5b2eb47552e04375534518
AMI-lesson <aceami113@gmail.com>
2025-04-02 14:03:04 +0900

    Add files via upload
    再提出2025/4/2

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,7 +4,7 @@ public class KatoExec_Chapter18 {
 
     public static void main(String[] args) {
 
-    	// 各子クラスのインスタンスを作成（それぞれに住所を渡す）
+        // 各子クラスのインスタンスを作成（それぞれに住所を渡す）
         String address = "東京都中野区〇×";
 
         // 各子クラスのインスタンスを作成
@@ -12,7 +12,12 @@ public class KatoExec_Chapter18 {
         KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18(address);
         KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18(address);
 
-        // execIntroduceメソッドを呼び出して情報を表示
+        // インスタンス生成後に setGivenName を呼び出す
+        taro.setGivenName();
+        ichiro.setGivenName();
+        hanako.setGivenName();
+
+        // execIntroduce メソッドを呼び出して情報を表示
         taro.execIntroduce();
         System.out.println(); // 空行を入れることで見やすくします
         ichiro.execIntroduce();
